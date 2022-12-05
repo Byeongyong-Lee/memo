82beceb76661837af4c982c246feddc6d126afb3
Lee <beongyong@naver.com>
2022-12-05 22:28:25 +0900

    init

---
+++ web/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <MainHeader/>
+  <MainBody/>
+  <MainFooter/>
+</template>
+
+<script>
+import MainHeader from "@/components/MainHeader";
+import MainBody from "@/components/MainFooter";
+import MainFooter from "@/components/MainBody";
+
+export default {
+  name: 'App',
+  components: {
+    MainBody,
+    MainFooter,
+    MainHeader
+  }
+}
+</script>
